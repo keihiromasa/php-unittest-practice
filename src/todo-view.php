5b9622ef4314e66480d1e8c28219c7228863d8a5
KeiHiromasa <hiromasa.kei@jp.fujitsu.com>
2016-07-19 12:37:03 +0900

    bootstrapの読み込み

--- src/todo-view.php
+++ src/todo-view.php
@@ -8,19 +8,23 @@ require_once dirname(__FILE__) . '/class/view/TODOViewElems.php';
         <title>TODO List</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     </head>
-    <body>
-        <?php
-        foreach ($todoList as $todo) {
-            echo '<div>' . $todo->get() . '</div>';
-        }
-        ?>
+</head>
+<body>
+    <?php
+    foreach ($todoList as $todo) {
+        echo '<div>' . $todo->get() . '</div>';
+    }
+    ?>
 
-        <form action="todo.php" method="post">
-            <input type="text" name="<?php echo TODOViewElems::TODO ?>" size="40">
-            <input type="submit" value="登録">
-            </p>
-        </form>
-    </body>
+    <form action="todo.php" method="post">
+        <input type="text" name="<?php echo TODOViewElems::TODO ?>" size="40">
+        <input type="submit" value="登録">
+        </p>
+    </form>
+</body>
 </html>
 
