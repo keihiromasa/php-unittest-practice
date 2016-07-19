3240dda18b6b29ebc09ca0a1f2e67245f671dffb
KeiHiromasa <higehigehigeo@gmail.com>
2016-07-19 21:46:05 +0900

    DBファイルの権限変更。テーブルがない時は作成するロジックを追加。

--- src/class/db/PDOFactory.php
+++ src/class/db/PDOFactory.php
@@ -10,7 +10,14 @@ class PDOFactory {
         //TODO mockかプロダクトオブジェクトを生成する
         $pdo = new PDO('sqlite:' . dirname(__FILE__) . '/mydb.db3');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        PDOFactory::prepareTable($pdo);
         return $pdo;
+    }
+
+    private static function prepareTable(PDO $pdo) {
+        $pdo->beginTransaction();
+        $pdo->exec("create table if not exists todo_list(todo nvarchar(100))");
+        $pdo->commit();
     }
 
 }
